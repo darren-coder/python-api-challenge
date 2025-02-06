ec8f8d94bd89b1dad66846aebd6f688a5f6614bf
Darren Holzhausen <darrenholzhausen@gmail.com>
2025-02-06 17:55:13 -0600

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "0be69387638a7fe69690654e573ed706"
+weather_api_key = 
 
 # Geoapify API Key
-geoapify_key = "fbd6ece1fd9041a3b0e4a2f4c557be68"
+geoapify_key = 
